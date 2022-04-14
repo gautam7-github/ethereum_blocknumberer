13b07178becea04454957ac423aa7dcc48a065d5
Gautam <61959145+gautam7-github@users.noreply.github.com>
2022-04-14 18:55:22 +0530

    optimised request flow

--- server.py
+++ server.py
@@ -5,17 +5,18 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 BLOCK = None
+web3Client = Web3(
+    Web3.HTTPProvider(
+        endpoint_uri=config.URL
+    )
+)
 
 
 @app.before_request
 def getBlock():
     global BLOCK
     try:
-        BLOCK = Web3(
-            Web3.HTTPProvider(
-                endpoint_uri=config.URL
-            )
-        ).eth.get_block_number()
+        BLOCK = web3Client.eth.get_block_number()
     except Exception as e:
         print(e)
 
